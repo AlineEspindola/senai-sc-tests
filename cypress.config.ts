71587f7828e15bd30d65292368ebc024cacade1e
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2025-09-08 15:07:15 -0300

    chore: Adiciona videos para testes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  video: true, 
+  videosFolder: 'cypress/videos', 
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.ts', 
     baseUrl: 'https://sc.senai.br/',
